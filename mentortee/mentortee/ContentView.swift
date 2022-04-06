9928499a0fc6dea94962f39751bcffc795c9ccda
태깅 <min4644@gmail.com>
2022-04-07 01:36:31 +0900

    tabview 탭 유지 기능 추가

--- mentortee/mentortee/ContentView.swift
+++ mentortee/mentortee/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 1
     var body: some View {
         VStack {
-            TabView(selection: .constant(1)) {
+            TabView(selection: $selection) {
                 MainPage().tabItem { Image(systemName: "house").environment(\.symbolVariants, .none) }.tag(1)
                 AnswerDetailView().tabItem { Image(systemName: "square.text.square").environment(\.symbolVariants, .none) }.tag(2)
                 Text("my qna").tabItem { Image(systemName: "doc.text").environment(\.symbolVariants, .none) }.tag(3)
