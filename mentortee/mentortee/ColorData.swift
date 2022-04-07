34dc0edda64fcd7c47ab940de001104ad00255ca
태깅 <min4644@gmail.com>
2022-04-07 23:49:04 +0900

    AnswerDetailView 리팩토링, 버그 개선

--- mentortee/mentortee/ColorData.swift
+++ mentortee/mentortee/ColorData.swift
@@ -23,6 +23,18 @@ extension Color {
 }
 
 extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
+
+extension Color {
     static let primaryColor = Color(hex: "#DC8D6C")
     static let subColor = Color(hex: "#E7C794")
     static let mainGreen = Color(hex: "#788C62")
