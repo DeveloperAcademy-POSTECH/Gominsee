84ac477bb7aa9d660b0a03e83ca2203e7fd84096
Rookie <jisu7408@gmail.com>
2022-04-10 18:02:17 +0900

    4.10 Question 수정완료

--- mentortee/mentortee/Views/QnAView/QuestionSub1.swift
+++ mentortee/mentortee/Views/QnAView/QuestionSub1.swift
@@ -9,39 +9,47 @@ import SwiftUI
 
 struct QuestionSub1: View {
     var body: some View {
-        VStack {
-            Image("WomanUser")
-                .resizable()
-                .frame(width: 200, height: 200)
+        
+        NavigationView {
             
-            Text("작성 완료!")
-                .font(.title2)
-                .padding()
-            
-            Text("스스로에게 한발짝 더 다가갔군요?")
-                .font(.subheadline)
-            Text("게시판에 내가 쓴 질문이 올라갔어요!")
-                .font(.subheadline)
-            
-            Button(action: {
-                //버튼을 클릭했을 때 실행되는 코드
+            VStack {
+                Image("WomanUser")
+                    .resizable()
+                    .frame(width: 200, height: 200)
                 
-            }){
-                //버튼의 보여지는 UI 코드
+                Text("작성 완료!")
+                    .font(.title2)
+                    .padding()
                 
-                Text("게시판으로 가볼래요?")
-                    .font(.title3)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding(.horizontal, 40)
-                    .padding(.vertical, 10)
-                    .background(Color.primaryColor)
-                    .cornerRadius(10)
+                Text("스스로에게 한발짝 더 다가갔군요?")
+                    .font(.subheadline)
+                Text("게시판에 내가 쓴 질문이 올라갔어요!")
+                    .font(.subheadline)
+                
+                NavigationLink(destination: QuestionDetailView() //추후 destination 수정 필요
+                    .navigationBarBackButtonHidden(true)
+                    .navigationBarHidden(true)) {
+                    
+                        Text("보관함으로 가볼래요?")
+                            .font(.title3)
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 40)
+                            .padding(.vertical, 10)
+                            .background(Color.primaryColor)
+                            .cornerRadius(10)
+                    }
+
+                }
+            .navigationBarHidden(true)
+
+                
             }
             
         }
+        
     }
-}
+
 
 struct QuestionSub1_Previews: PreviewProvider {
     static var previews: some View {
