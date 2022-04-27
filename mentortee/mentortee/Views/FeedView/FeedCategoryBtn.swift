c65e00c55404e78acd27c07ec604154024ab1d22
JI SOO <dlwltn815@gmail.com>
2022-04-27 16:23:28 +0900

    Category Btn 리팩토링

--- mentortee/mentortee/Views/FeedView/FeedCategoryBtn.swift
+++ mentortee/mentortee/Views/FeedView/FeedCategoryBtn.swift
@@ -1,210 +1,35 @@
-//
-//  FeedCategoryBtn.swift
-//  mentortee
-//
-//  Created by 이지수 on 2022/04/06.
-//
-
 import SwiftUI
 
+enum categoryBtn: String {
+    case all = "전체"
+    case veiwPoint = "가치관"
+    case career = "진로"
+    case preference = "취향"
+    case hobby = "취미"
+    case concern = "고민"
+    case secret = "비밀"
+    case reflection = "회고"
+    case habit = "습관"
+}
+
 struct FeedCategoryBtn: View {
-    @State var click = [true, false, false, false, false, false, false, false, false]
-    let screenHeight = UIScreen.main.bounds.height
-
-    // 카테고리 - 전체,가치관, 진로, 취향, 취미, 고민, 비밀, 회고, 습관
+    @State var categoryList = ["전체" ,"가치관", "진로", "취향", "취미", "고민", "비밀", "회고", "습관"]
+    @State var currentIdx = 0
+    
     var body: some View {
-        HStack {
-            ScrollView(.horizontal,
-                showsIndicators: false) {
-                HStack (spacing: 5) {
-                    Button("전체") {
-                        withAnimation(.easeInOut) {
-                            click[0] = true
-                            for i in 0..<click.count {
-                                if i != 0 {
-                                    click[i] = false
-                                }
-                            }
-                        }
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(categoryList, id:\.self) { value in
+                    Button(value) {
+                        currentIdx = categoryList.firstIndex(of: value)!
                     }
-                        .frame(width: 60, height: 30)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[0] ? Color.primaryColor : Color.gray))
-                        .foregroundColor(Color.white)
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[1] = true
-                            for i in 0..<click.count {
-                                if i != 1 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("가치관")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[1] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[2] = true
-                            for i in 0..<click.count {
-                                if i != 2 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("진로")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[2] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[3] = true
-                            for i in 0..<click.count {
-                                if i != 3 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("취향")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[3] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[4] = true
-                            for i in 0..<click.count {
-                                if i != 4 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("취미")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[4] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[5] = true
-                            for i in 0..<click.count {
-                                if i != 5 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("고민")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[5] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[6] = true
-                            for i in 0..<click.count {
-                                if i != 6 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("비밀")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[6] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[7] = true
-                            for i in 0..<click.count {
-                                if i != 7 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("회고")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[7] ? Color.primaryColor : Color.gray))
-
-                    Button(action: {
-                        withAnimation(.easeInOut) {
-                            click[8] = true
-                            for i in 0..<click.count {
-                                if i != 8 {
-                                    click[i] = false
-                                }
-                            }
-                        }
-                    }, label: {
-                            Text("습관")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 15))
-                                .frame(width: 60, height: 35, alignment: .center)
-                        })
-                        .padding()
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(click[8] ? Color.primaryColor : Color.gray))
+                    .frame(width: 60, height: 30, alignment: .center)
+                    .foregroundColor(Color.white)
+                    .background(RoundedRectangle(cornerRadius: 40)
+                        .fill( currentIdx == categoryList.firstIndex(of: value)!  ? Color.primaryColor : Color.gray))
                 }
-                    .background(Color.backgroundColor)
-
             }
         }
-            .padding(.all, 16)
     }
 }
 
@@ -213,5 +38,3 @@ struct FeedCategoryBtn_Previews: PreviewProvider {
         FeedCategoryBtn()
     }
 }
-
-
