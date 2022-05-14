36092d280d5e86324e4d29696a9ec2b46ea1a284
Rookie <jisu7408@gmail.com>
2022-05-14 20:42:43 +0900

    05.14 Refactor (4) - FeedView
    [FeedView]
    
    1. ScrollView 프레임 조절
    : ContentView에 FeedView를 걸면, 하단의 tab bar까지 Scroll영역이 잡히는 버그가 발생함
    
    2. Offset 삭제 및 약간의 padding 조정
    : 카테고리 버튼과 간격이 너무 없어 UX 가 좋지않다고 생각함.

--- mentortee/mentortee/Views/FeedView/FeedView.swift
+++ mentortee/mentortee/Views/FeedView/FeedView.swift
@@ -1,46 +1,48 @@
-//
-//  FeedView.swift
-//  mentortee
-//
-//  Created by 이지수 on 2022/04/08.
-//
-
 import SwiftUI
 
 struct FeedView: View {
     @State private var showModal = false
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            ZStack {
-                VStack {
-                    FeedCategoryBtn()
-                    ScrollView {
-                        VStack {
-                            FeedQuestion(category: "가치관",
-                                otherThoughts: "다른 생각 9개",
-                                question: "당신이 생각하는 이상적인 삶은 어떤 모습인가요?",
-                                questionOwner: "Chemi",
-                                nickname: "Chemi"
-                            )
-                            FeedQuestion(category: "취미",
-                                otherThoughts: "다른 생각 10개",
-                                question: "요즘 가장 즐기고 있는 일은 무엇인가요?",
-                                questionOwner: "Brown",
-                                nickname: "Chemi"
-                            )
-                            FeedQuestion(category: "습관",
-                                otherThoughts: "다른 생각 120개",
-                                question: "오늘 아침에 일어나자마자 한 행동은 무엇인가요?",
-                                questionOwner: "Daon",
-                                nickname: "Chemi"
-                            )
-                        }
+        
+        
+        GeometryReader { geo in
+            VStack(alignment: .leading, spacing: 0) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color.backgroundColor)
+                        .ignoresSafeArea()
+                    
+                    VStack {
+                        FeedCategoryBtn()
+                            .padding()
+                        
+                        ScrollView {
+                            VStack {
+                                FeedQuestion(category: "가치관",
+                                    otherThoughts: "다른 생각 9개",
+                                    question: "당신이 생각하는 이상적인 삶은 어떤 모습인가요?",
+                                    questionOwner: "Chemi",
+                                    nickname: "Chemi"
+                                )
+                                FeedQuestion(category: "취미",
+                                    otherThoughts: "다른 생각 10개",
+                                    question: "요즘 가장 즐기고 있는 일은 무엇인가요?",
+                                    questionOwner: "Brown",
+                                    nickname: "Chemi"
+                                )
+                                FeedQuestion(category: "습관",
+                                    otherThoughts: "다른 생각 120개",
+                                    question: "오늘 아침에 일어나자마자 한 행동은 무엇인가요?",
+                                    questionOwner: "Daon",
+                                    nickname: "Chemi"
+                                )
+                            }
+                        }.frame(height: geo.size.height * 0.88)
                     }
                 }
             }
-                .offset(y: -30)
-                .background(Color.backgroundColor)
+
         }
     }
 }
