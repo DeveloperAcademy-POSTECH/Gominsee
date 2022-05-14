db5840dcd9b3052553a0dfa260269235f922bb80
Rookie <jisu7408@gmail.com>
2022-05-14 20:00:10 +0900

    05.14 Refactor(2)
    [SettingView]
    MyPageView의 관심 카테고리 이동 bar를 SettingView로 추가함.
    
    사유 : 기존 관심 카테고리 이동bar는 List 형태로 SettingView에 사용시 통일성을 높일 수 있어서 굳이 MypageView 자체에 있을 이유는 없다고 판단함
    
    [MypageView]
    기존에 여러개의 ChildView로 구성하던 구조를 없애고 하나의 ParentView 내에 작성함
    
    사유 : MypageView는 사용자 정보를 담고있는 View이므로 만약 이 struct가 child View로 사용될 경우 사용자 정보를 Parameter 사용하여 View업데이트가 용이하다고 판단함.
    
    사용자 정보의 예시
    username, myQuestionCount, myAnswerCount, userLV 등

--- mentortee/mentortee/Views/MyPageView/MyPageView.swift
+++ mentortee/mentortee/Views/MyPageView/MyPageView.swift
@@ -1,124 +1,83 @@
-//
-//  MyPageView.swift
-//  mentortee
-//
-//  Created by 김민택 on 2022/04/10.
-//
+
 
 import SwiftUI
 
-struct MyInfo: View {
-    var username: String
-    var body: some View {
-        HStack {
-            HStack {
-                Text(username).bold()
-                Text("님").padding(.leading, -5)
-            }.font(.system(size: 24))
-            Spacer()
-        }
-    }
-}
-
-struct MyQna: View {
-    let screenWidth = UIScreen.main.bounds.width
-    var cntMyQuestion: Int
-    var cntMyAnswer: Int
-    @Binding var firstNaviLinkActive: Bool
-
-    var body: some View {
-        HStack {
-            NavigationLink(destination: QuestionCardView(firstNaviLinkActive: $firstNaviLinkActive)
-                    .navigationBarHidden(true)) {
-                VStack {
-                    Text("내가 만든 질문").font(.system(size: 14)).bold()
-                    Text("\(cntMyQuestion)").font(.system(size: 30)).bold()
-                }.frame(width: (screenWidth - 50) / 2)
-            }
-                .foregroundColor(.black)
-
-            Divider()
-
-            NavigationLink(destination: AnswerCardView()
-                    .navigationBarHidden(true)) {
-                VStack {
-                    Text("내가 답한 질문").font(.system(size: 14)).bold()
-                    Text("\(cntMyAnswer)").font(.system(size: 30)).bold()
-                }.frame(width: (screenWidth - 50) / 2)
-            }
-                .foregroundColor(.black)
-
-        }.frame(maxWidth: .infinity, maxHeight: 90).background(RoundedRectangle(cornerRadius: 10).fill(Color.white)).shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
-    }
-}
-
-struct MyCharacter: View {
-    var userLV = 1
-    var cntRequireQuestion = 1
-    var cntRequireAnswer = 2
-
-    var body: some View {
-        ZStack {
-            VStack {
-                Image("character_lv1").shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
-                Spacer()
-            }
-            VStack {
-                HStack {
-                    HStack {
-                        Text("곰민").font(.system(size: 20)).bold()
-                        Text("Lv. \(userLV)").font(.system(size: 14))
-                    }.frame(alignment: .leading).padding(10).background(RoundedRectangle(cornerRadius: 10).fill(Color.white)).shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
-                    Spacer()
-                }
-                Spacer().frame(height: 5)
-                HStack {
-                    HStack {
-                        Text("다음 성장까지")
-                        Text("질문 \(cntRequireQuestion)개, 답변 \(cntRequireAnswer)개").bold()
-                        Text("가 필요해요!")
-                    }.font(.system(size: 14)).padding(10).background(RoundedRectangle(cornerRadius: 10).fill(Color.white)).shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
-                    Spacer()
-                }
-                Spacer()
-            }.frame(maxWidth: .infinity, alignment: .leading)
-                .padding(25)
-        }
-    }
-}
-
-struct favoriteCategory: View {
-    var body: some View {
-        NavigationLink(destination: CategoryView()
-                .navigationBarHidden(true)) {
-            HStack {
-                Text("관심 카테고리").font(.system(size: 16)).padding(.leading, 10)
-                Spacer()
-                Image(systemName: "chevron.right").padding(.trailing, 10)
-            }
-                .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
-                .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
-                .foregroundColor(.black)
-                .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
-        }
-    }
-}
-
 struct MyPageView: View {
     @Binding var firstNaviLinkActive: Bool
+    var username : String = "Chemi"
+    var myQuestionCount : Int = 2
+    var myAnswerCount : Int = 3
+    var userLV : Int = 1
+    let screenWidth = UIScreen.main.bounds.width
+    
     var body: some View {
         VStack {
-            MyInfo(username: "Chemi")
-            MyQna(cntMyQuestion: 2, cntMyAnswer: 3, firstNaviLinkActive: $firstNaviLinkActive)
-            MyCharacter()
-            favoriteCategory()
-            Spacer()
-        }
-            .offset(y: -40)
-            .padding(.horizontal, 16)
+            
+            Text("\(username)의 질문상자")
+                .font(.system(size: 24).weight(.bold))
+            
+            HStack {
+                NavigationLink(destination: QuestionCardView(firstNaviLinkActive: $firstNaviLinkActive).navigationBarHidden(true)) {
+                        VStack {
+                            Text("내가 만든 질문").font(.system(size: 14)).bold()
+                            Text("\(myQuestionCount)").font(.system(size: 30)).bold()
+                        }
+                        .frame(width: (screenWidth - 50) / 2)
+                    }
+                    .foregroundColor(.black)
+                
+                Divider()
+                
+                NavigationLink(destination: AnswerCardView().navigationBarHidden(true)) {
+                        VStack {
+                            Text("내가 답한 질문").font(.system(size: 14)).bold()
+                            Text("\(myAnswerCount)").font(.system(size: 30)).bold()
+                        }
+                        .frame(width: (screenWidth - 50) / 2)
+                    }
+                    .foregroundColor(.black)
+                    .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
+            }
+            .frame(width: 350, height: 100)
+            .background(.white)
+            .cornerRadius(10)
+            
+            ZStack {
+                VStack {
+                    Image("character_lv1").shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
+                    Spacer()
+                }
+                VStack {
+                    HStack {
+                        HStack {
+                            Text("곰민").font(.system(size: 20)).bold()
+                            Text("Lv. \(userLV)").font(.system(size: 14))
+                        }.frame(alignment: .leading).padding(10)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(color: .black.opacity(0.1), radius: 20, x: 0, y: 0)
+                        Spacer()
+                    }
+                    HStack {
+                        HStack {
+                            Text("다음 성장까지")
+                            Text("질문 \(myQuestionCount)개, 답변 \(myAnswerCount)개").font(.system(size: 16).weight(.bold))
+                            Text("가 필요해요!")
+                        }.font(.system(size: 14)).padding(10).background(RoundedRectangle(cornerRadius: 10).fill(Color.white)).shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
+                        Spacer()
+                    }
+                    Spacer()
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(25)
+            }
+            
+            }
+            .padding(.top, 15)
             .background(Color.backgroundColor)
+        }
     }
-}
+
+
 
 struct MyPageView_Previews: PreviewProvider {
     static var previews: some View {
--- mentortee/mentortee/Views/MyPageView/SettingsView.swift
+++ mentortee/mentortee/Views/MyPageView/SettingsView.swift
@@ -12,6 +12,9 @@ struct SettingsView: View {
 
     var body: some View {
         List {
+            NavigationLink(destination: CategoryView().navigationBarHidden(true)){
+                Text("관심 카테고리")}
+            .foregroundColor(.black)
             Text("닉네임 변경")
             Text("로그아웃")
             Text("버전 정보 1.0.0")
@@ -28,6 +31,7 @@ struct SettingsView: View {
         )
     }
 }
+
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
