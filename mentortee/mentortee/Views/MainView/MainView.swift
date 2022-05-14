96191b2a60338a59e174f6b162a460901121bccd
Rookie <jisu7408@gmail.com>
2022-05-14 20:43:48 +0900

    05.14 Refactor(3) - MainView
    [MainView]
    
    TextEditor 탭 시, 질문 카드 opacity 감소 + 줄 조정
    
    사유 : 사용자가 질문에 집중할 수 있는 UX를 의도함.
    
    추가 개발 사항 : TextEditor 탭 시, 키보드가 올라와 question card 비율이 깨지는 문제를 수정 하지 못함

--- mentortee/mentortee/Views/MainView/MainView.swift
+++ mentortee/mentortee/Views/MainView/MainView.swift
@@ -1,112 +1,125 @@
 import SwiftUI
 
 struct MainView: View {
+    
     var dailyQuestion = "Chemi님은 뭘 할 때 행복한가요 ?"
     var categoryList = ["가치관"]
     var today = Date()
-    @State private var cardHeight = UIScreen.main.bounds.height * 0.45
-    @State private var cardTextHeight = UIScreen.main.bounds.height * 0.2
+    
     @State private var answerText = "질문에 대한 나의 생각을 적어보세요."
     @State private var answerColor = Color.black.opacity(0.2)
-    @State private var tapTextEditor = false
     @State private var showAlert = false
+    @State private var cardHeight = UIScreen.main.bounds.height * 0.45
+    @State private var cardTextHeight = UIScreen.main.bounds.height * 0.2
+    @State private var cardOpacity : Double = 1
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            
+            ZStack {
+                Color.backgroundColor
+                    .ignoresSafeArea()
+                
+                ScrollView {
+                    VStack(alignment: .center, spacing: -10) {
+                        
+                        VStack(alignment: .leading){
+                            Text("\(today, formatter: MainView.dateformat)")
+                                .bold()
+                            Text(dailyQuestion)
+                                .bold()
+                                .font(.system(size: 35))
+                                .frame(width: nil, height: geo.size.height * 0.2)
+                                .minimumScaleFactor(0.5)
+                            
+                            HStack {
+                                ForEach(categoryList, id: \.self) { value in
+                                    Text(value)
+                                        .padding(.vertical, 3)
+                                        .padding(.horizontal, 10)
+                                        .font(.system(size: 14))
+                                        .foregroundColor(Color.primaryColor)
+                                        .background(RoundedRectangle(cornerRadius: 10))
+                                }
+                                Spacer()
+                            }
+                            
+                            VStack{
+                                
+                                Button("제출하기", action: answerText.count == 0 || answerText == "질문에 대한 나의 생각을 적어보세요." ? {} : {
+                                    showAlert = true
+                                    hideKeyboard()
+                                    answerColor = Color.black.opacity(0.2)
+                                    answerText = "질문에 대한 나의 생각을 적어보세요."
+                                    cardHeight = geo.size.height * 0.45
+                                    cardTextHeight = geo.size.height * 0.2
+                                    
+                                })
+                                .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                                .foregroundColor(answerText.count == 0 || answerText == "질문에 대한 나의 생각을 적어보세요." ? Color.mainGreen.opacity(0.4) : Color.mainGreen)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10))
+                                .alert(isPresented: $showAlert) {
+                                    Alert(
+                                        title: Text("제출 완료"),
+                                        message: Text("마이페이지에서 확인할 수 있어요!")
+                                    )
+                                }
+                            }
+                            .frame(width: geo.size.width * 0.8,alignment: .trailing)
+                            .padding(.top, 50)
+                        }
+                        .padding()
+                        .frame(width: geo.size.width * 0.87, height: geo.size.height * 0.5)
+                        .foregroundColor(.white)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(
+                                    colors: [Color.primaryColor, Color.subIvory]),
+                                startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .cornerRadius(10)
+                        .opacity(cardOpacity)
+                        
+                        
+                        TextEditor(text: $answerText)
+                            .padding()
+                            .frame(width: geo.size.width * 0.87, height: geo.size.width * 0.5, alignment: .top)
+                            .foregroundColor(answerColor)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0)
+                            .onTapGesture {
+                                if answerText == "질문에 대한 나의 생각을 적어보세요."{
+                                    answerText = ""
+                                }
+                                answerColor = Color.black
+                                cardOpacity = 0.7
 
+                            }
+                        Spacer()
+                    }
+                }.padding(.top,15)
+            }
+            .onTapGesture {
+                hideKeyboard()
+                cardOpacity = 1.0
+            }
+        }
+    }
+    
+    
     static let dateformat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko-KR")
         formatter.dateFormat = "YY년 M월 d일 (eeee)"
         return formatter
     }()
+    
+}
 
-    var body: some View {
-        GeometryReader { geometry in
-            VStack(alignment: .center, spacing: 0) {
-                VStack(alignment: .leading) {
-                    Text("\(today, formatter: MainView.dateformat)")
-                        .bold()
-                        .padding(.top, 20)
-                    Text(dailyQuestion)
-                        .dailyQuestion()
-                        .frame(width: geometry.size.width * 0.8, height: cardTextHeight, alignment: .leading)
-
-                    HStack {
-                        ForEach(categoryList, id: \.self) { value in
-                            Text(value)
-                                .dailyCategoryStyle()
-                        }
-                    }
-
-                    Button("제출하기", action: answerText.count == 0
-                            || answerText == "질문에 대한 나의 생각을 적어보세요."
-                            ? { }
-                        : {
-                            showAlert = true
-                            answerText = "질문에 대한 나의 생각을 적어보세요."
-                            hideKeyboard()
-                            answerColor = Color.black.opacity(0.2)
-                            cardHeight = UIScreen.main.bounds.height * 0.45
-                            cardTextHeight = geometry.size.height * 0.2
-                        })
-                        .foregroundColor(answerText.count == 0
-                            || answerText == "질문에 대한 나의 생각을 적어보세요."
-                            ? Color.mainGreen.opacity(0.4)
-                        : Color.mainGreen)
-                        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
-                        .background(
-                        RoundedRectangle(cornerRadius: 10))
-                        .alert(isPresented: $showAlert) {
-                        Alert(
-                            title: Text("제출 완료"),
-                            message: Text("마이페이지에서 확인할 수 있어요!"),
-                            primaryButton: .default(Text("보러가기")),
-                            secondaryButton: .cancel(Text("완료"), action: {
-                                    tapTextEditor = false
-                                }))
-                    }
-                        .padding(.top, 50)
-                        .frame(width: geometry.size.width * 0.8, alignment: .trailing)
-                }
-                    .padding()
-                    .background(LinearGradient(
-                    gradient: Gradient(colors: [Color.primaryColor, Color.subIvory]),
-                    startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-
-                TextEditor(text: $answerText)
-                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.2)
-                    .onTapGesture {
-                    answerText = ""
-                    answerColor = Color.black
-                    cardHeight = geometry.size.height * 0.1
-                    cardTextHeight = geometry.size.height * 0.1
-                }
-                    .foregroundColor(answerColor)
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 10)
-                        .foregroundColor(.white)
-                        .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 0))
-                    .onChange(of: answerText) { value in
-                    if answerText.count == 0 {
-                        tapTextEditor = false
-                    } else {
-                        tapTextEditor = true
-                    }
-                }
-            }
-                .frame(width: geometry.size.width, height: geometry.size.height)
-                .background(Color.backgroundColor.ignoresSafeArea())
-                .onTapGesture {
-                hideKeyboard()
-                cardHeight = geometry.size.height * 0.45
-                cardTextHeight = geometry.size.height * 0.2
-            }
-        }
-    }
-
-    struct MainView_Previews: PreviewProvider {
-        static var previews: some View {
-            MainView()
-        }
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
     }
 }
