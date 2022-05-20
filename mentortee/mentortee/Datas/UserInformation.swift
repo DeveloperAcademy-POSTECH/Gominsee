729cf7bf4870f8334eb2e99dde2fdb38574c2480
Rookie <jisu7408@gmail.com>
2022-05-20 19:59:49 +0900

    05.20 Refactor - Data
    1. MyPageData 구조체 생성
    - MyPageView에 사용될 정보들을 담는 틀을 만듬
    (유저이름, 질문과 답변의 수, 레벨)
    
    2. UserInformation 클래스 생성
    - DataFlow 실험용으로 생성하였으며 현재는 마이페이지 뷰 생성에 필요한 데이터만 담고 있음.
    추후 최종 구조를 이 방식으로 따라갈지에 대한 논의가 필요함.

---
+++ mentortee/mentortee/Datas/UserInformation.swift
@@ -0,0 +1,12 @@
+//
+//  UserInformation.swift
+//  mentortee
+//
+//  Created by Jisu Jang on 2022/05/20.
+//
+
+import Foundation
+
+final class UserInformation : ObservableObject {
+    @Published var myPageData : MyPageData = MyPageData(username: "Chemi", myQuestionCount: 0, myAnswerCount: 0, userLV: 0)
+}
