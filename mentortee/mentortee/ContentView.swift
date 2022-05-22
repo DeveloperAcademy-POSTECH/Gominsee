67cb755b8a1a10ff1017f24451d089c692f26421
JI SOO <dlwltn815@gmail.com>
2022-05-22 20:49:41 +0900

    Feat: Login, Logout 화면 전환

--- mentortee/mentortee/ContentView.swift
+++ mentortee/mentortee/ContentView.swift
@@ -1,37 +1,48 @@
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
     @State private var selection = 1
     @State private var isShowingDetailView = false
     @State var firstNaviLinkActive: Bool = false
     @EnvironmentObject var userInformation : UserInformation
-
+    @AppStorage("log_status") var log_Status = false
+    
     var body: some View {
-        NavigationView {
-            TabView(selection: $selection) {
-                MainView().tabItem { Image(systemName: IconName.house).environment(\.symbolVariants, .none) }.tag(1)
-                FeedView().tabItem { Image(systemName: IconName.textSquare).environment(\.symbolVariants, .none) }.tag(2)
-
-                MyPageView(firstNaviLinkActive: $firstNaviLinkActive, myPageData: userInformation.myPageData).tabItem { Image(systemName: IconName.person).environment(\.symbolVariants, .none) }.tag(3)
+        ZStack {
+            if log_Status {
+                NavigationView {
+                    TabView(selection: $selection) {
+                        MainView().tabItem { Image(systemName: IconName.house).environment(\.symbolVariants, .none) }.tag(1)
+                        FeedView().tabItem { Image(systemName: IconName.textSquare).environment(\.symbolVariants, .none) }.tag(2)
+                        
+                        MyPageView(firstNaviLinkActive: $firstNaviLinkActive, myPageData: userInformation.myPageData).tabItem { Image(systemName: IconName.person).environment(\.symbolVariants, .none) }.tag(3)
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Text("🐼고민씨")
+                                .padding(.leading, 16)
+                                .font(.system(size: 22).weight(.bold))
+                            .foregroundColor(.primaryColor) }
+                        
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(destination: getDestination(), isActive: $firstNaviLinkActive) {
+                                Image(systemName: selection == 3 ? IconName.settings : IconName.pencil)
+                                    .padding(.all, 16)
+                                    .font(.system(size: 20))
+                                .foregroundColor(.mainGreen) } }
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
+                }
+                .onAppear() {
+                    print("ContentView 시작")
+                }
+            } else {
+                LogInView()
             }
-                .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Text("🐼고민씨")
-                        .padding(.leading, 16)
-                        .font(.system(size: 22).weight(.bold))
-                        .foregroundColor(.primaryColor) }
-
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: getDestination(), isActive: $firstNaviLinkActive) {
-                        Image(systemName: selection == 3 ? IconName.settings : IconName.pencil)
-                            .padding(.all, 16)
-                            .font(.system(size: 20))
-                            .foregroundColor(.mainGreen) } }
-            }
-                .navigationBarTitleDisplayMode(.inline)
         }
     }
-
+    
     func getDestination() -> AnyView {
         if (selection == 3) {
             return AnyView(SettingsView())
--- mentortee/mentortee/Views/MyPageView/SettingsView.swift
+++ mentortee/mentortee/Views/MyPageView/SettingsView.swift
@@ -1,32 +1,49 @@
 import SwiftUI
+import FirebaseAuth
 
 struct SettingsView: View {
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
-
+    @AppStorage("log_status") var log_Status = false
+    
     var body: some View {
         List {
             NavigationLink(destination: FavoriteCategoryView()
-                    .navigationBarBackButtonHidden(true)
-                    .navigationTitle(Text(TextName.favoriteCategoryText))) {
-                Text(TextName.favoriteCategoryText) }
+                .navigationBarBackButtonHidden(true)
+                .navigationTitle(Text(TextName.favoriteCategoryText))) {
+                    Text(TextName.favoriteCategoryText) }
                 .foregroundColor(.mainBlack)
             Text("닉네임 변경")
             Text("로그아웃")
+            Button(action: {
+                do {
+                    try Auth.auth().signOut()
+                } catch let signOutError as NSError {
+                    print("Error signing out: %@", signOutError)
+                }
+            }, label: {
+                Text("로그아웃")
+                    .onTapGesture {
+                        DispatchQueue.global(qos: .background).async {
+                            try? Auth.auth().signOut()
+                        }
+                        withAnimation(.easeInOut) {
+                            log_Status = false
+                        }
+                    }
+            })
             Text("버전 정보 1.0.0")
         }
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading: Button(action: {
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: Button(action: {
             self.mode.wrappedValue.dismiss()
         }) {
-                Image(systemName: IconName.backward)
-                    .padding(.all, 16)
-                    .font(.system(size: 20))
-                    .foregroundColor(.mainBlack)
-            }
-        )
+            Image(systemName: IconName.backward)
+                .padding(.all, 16)
+                .font(.system(size: 20))
+                .foregroundColor(.mainBlack)
+        })
     }
 }
-
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
