8cc8b8a54c910d1bbb752b8962dffb8c98f82668
MMMIIIN <wbsldj7645@naver.com>
2022-05-24 21:58:11 +0900

    Design: 불필요한 UI삭제
    - 질문 추가 후 Alert표시 후 메인으로 돌아오기

--- mentortee/mentortee/Views/QnAView/QuestionSub1.swift
+++ mentortee/mentortee/Views/QnAView/QuestionSub1.swift
@@ -1,45 +1,45 @@
-import SwiftUI
-
-struct QuestionSub1: View {
-    @Binding var firstNaviLinkActive: Bool
-
-    var body: some View {
-
-        ZStack {
-            Color.backgroundColor
-                .ignoresSafeArea()
-
-            VStack {
-                Image.womanUser
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                Text(TextName.completeText)
-                    .font(.title2)
-                    .padding()
-                Text(TextName.oneStepYourself)
-                    .font(.subheadline)
-                Text(TextName.uploadBoardText)
-                    .font(.subheadline)
-
-                NavigationLink(destination: QuestionCardView(firstNaviLinkActive: $firstNaviLinkActive)
-                        .navigationBarHidden(true)) {
-                    Text(TextName.goLocker)
-                        .bold()
-                        .font(.title3)
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 40)
-                        .padding(.vertical, 10)
-                        .background(Color.primaryColor)
-                        .cornerRadius(10)
-                }
-            }
-        }
-    }
-}
-
-
-struct QuestionSub1_Previews: PreviewProvider {
-    static var previews: some View {
-        QuestionSub1(firstNaviLinkActive: .constant(true))
-    }
-}
+//import SwiftUI
+//
+//struct QuestionSub1: View {
+//    @Binding var firstNaviLinkActive: Bool
+//
+//    var body: some View {
+//
+//        ZStack {
+//            Color.backgroundColor
+//                .ignoresSafeArea()
+//
+//            VStack {
+//                Image.womanUser
+//                    .resizable()
+//                    .frame(width: 200, height: 200)
+//                Text(TextName.completeText)
+//                    .font(.title2)
+//                    .padding()
+//                Text(TextName.oneStepYourself)
+//                    .font(.subheadline)
+//                Text(TextName.uploadBoardText)
+//                    .font(.subheadline)
+//
+//                NavigationLink(destination: QuestionCardView(firstNaviLinkActive: $firstNaviLinkActive)
+//                        .navigationBarHidden(true)) {
+//                    Text(TextName.goLocker)
+//                        .bold()
+//                        .font(.title3)
+//                        .foregroundColor(.white)
+//                        .padding(.horizontal, 40)
+//                        .padding(.vertical, 10)
+//                        .background(Color.primaryColor)
+//                        .cornerRadius(10)
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//
+//struct QuestionSub1_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QuestionSub1(firstNaviLinkActive: .constant(true))
+//    }
+//}
--- mentortee/mentortee/Views/QnAView/QuestionSub2.swift
+++ mentortee/mentortee/Views/QnAView/QuestionSub2.swift
@@ -1,42 +1,42 @@
-import SwiftUI
-
-struct QuestionSub2: View {
-    @Binding var firstNaviLinkActive: Bool
-
-    var body: some View {
-        ZStack {
-            Color.backgroundColor
-                .ignoresSafeArea()
-            VStack {
-                Image.manUser
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                Text(TextName.completeText)
-                    .font(.title2)
-                    .padding()
-                Text(TextName.oneStepYourself)
-                    .font(.subheadline)
-                Text(TextName.stackMyQuestion)
-                    .font(.subheadline)
-
-                NavigationLink(destination: QuestionCardView(firstNaviLinkActive: $firstNaviLinkActive)
-                        .navigationBarHidden(true)) {
-                    Text(TextName.goLocker)
-                        .font(.title3)
-                        .bold()
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 40)
-                        .padding(.vertical, 10)
-                        .background(Color.primaryColor)
-                        .cornerRadius(10)
-                }
-            }
-        }
-    }
-}
-
-struct QuestionSub2_Previews: PreviewProvider {
-    static var previews: some View {
-        QuestionSub2(firstNaviLinkActive: .constant(true))
-    }
-}
+//import SwiftUI
+//
+//struct QuestionSub2: View {
+//    @Binding var firstNaviLinkActive: Bool
+//
+//    var body: some View {
+//        ZStack {
+//            Color.backgroundColor
+//                .ignoresSafeArea()
+//            VStack {
+//                Image.manUser
+//                    .resizable()
+//                    .frame(width: 200, height: 200)
+//                Text(TextName.completeText)
+//                    .font(.title2)
+//                    .padding()
+//                Text(TextName.oneStepYourself)
+//                    .font(.subheadline)
+//                Text(TextName.stackMyQuestion)
+//                    .font(.subheadline)
+//
+//                NavigationLink(destination: QuestionCardView(firstNaviLinkActive: $firstNaviLinkActive)
+//                        .navigationBarHidden(true)) {
+//                    Text(TextName.goLocker)
+//                        .font(.title3)
+//                        .bold()
+//                        .foregroundColor(.white)
+//                        .padding(.horizontal, 40)
+//                        .padding(.vertical, 10)
+//                        .background(Color.primaryColor)
+//                        .cornerRadius(10)
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct QuestionSub2_Previews: PreviewProvider {
+//    static var previews: some View {
+//        QuestionSub2(firstNaviLinkActive: .constant(true))
+//    }
+//}
