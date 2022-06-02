6e1c248f8a1e0c6c42b0f546eaa6b23147ee46cf
MMMIIIN <wbsldj7645@naver.com>
2022-06-02 17:40:34 +0900

    Refactor: AnswerCardList
    - 불필요한 코드 삭제

--- mentortee/mentortee/Views/FeedView/Answer/AnswerCardList.swift
+++ mentortee/mentortee/Views/FeedView/Answer/AnswerCardList.swift
@@ -1,11 +1,6 @@
 import SwiftUI
 
-// 유저가 dailyQuestion에 답한다면, 그 답변이 유저아이디로 분류되어서 내가답한 질문으로 들어간다. 
-
 struct AnswerCardList: View {
-    @State private var currentIdx: Category = .all
-    @State private var showModal = false
-    @EnvironmentObject var userInfo: UserInformation
     @EnvironmentObject var firestoreData: FireStoreManager
 
     var body: some View {
@@ -13,48 +8,11 @@ struct AnswerCardList: View {
             Rectangle().foregroundColor(.backgroundColor).ignoresSafeArea()
             ScrollView {
                 VStack(alignment: .leading) {
-//                    AnswerCategoryButtonList()
                     ForEach(firestoreData.userAnswerList, id: \.id) { data in
                         AnswerCard(answerData: data, currentIdx: Category.all)
-                        
                     }
-//                    AnswerCardList()
-//                        .frame(height: geo.size.height * 0.88)
                 }
             }
         }
     }
-
-    private func AnswerCategoryButtonList() -> some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack {
-                ForEach(Category.allCases, id: \.self) { value in
-                    Button(value.rawValue) {
-                        currentIdx = value
-                    }
-                        .frame(width: 60, height: 30, alignment: .center)
-                        .foregroundColor(Color.white)
-                        .background(RoundedRectangle(cornerRadius: 40)
-                            .fill(currentIdx == value ? Color.primaryColor : Color.gray))
-                }
-            }
-        }
-            .padding()
-    }
-
-//    private func AnswerCardList() -> some View {
-//        ScrollView {
-//            VStack{
-//                ForEach(firestoreData.userAnswerList, id: \.self) { data in
-//                    if currentIdx == .all {
-//                        AnswerCard(questionData: data, currentIdx: currentIdx)
-//                    } else if data.category.contains(currentIdx) {
-//                        AnswerCard(questionData: data, currentIdx: currentIdx)
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-    
 }
